d67270d5b2b81ba72659d37c099e1c84832b83f5
ThousandAI <ceo@thousandai.com>
2022-06-20 01:46:30 +0800

    apcs0001-First Commit

---
+++ class01/src/class01.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : class01.c
+ Author      : Thousand AI
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	printf("Hello APCS!");
+	return 0;
+}
