eda4648c8b235f4472a89a59511e2869121df973
ThousandAI <ceo@thousandai.com>
2022-06-20 17:55:19 +0800

    class01 done

--- class01/src/class01.c
+++ class01/src/class01.c
@@ -9,9 +9,22 @@
  */
 
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(void) {
-	printf("Hello APCS!");
+	// 註解
+	// 宣告變數
+	char ch;
+	int number;
+	float score;
+	ch = 'A'; // 指定運算子 (賦予 assign) 從右往左看
+	// char ch = 'A' 可以宣告時直接賦予值
+	number = 12;
+	score = 36.5;
+	printf("%c\n", ch); // 所對應的變數型態必須要正確
+	printf("%d\n", ch); // ASCII code
+	printf("%d\n", number);
+	printf("%d\n", score); // score 是浮點數而若使用整數的方式印出，會產生錯誤的數值
+	printf("%f\n", score);
+	printf("%.2f",score);
 	return 0;
 }
