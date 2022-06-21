3994290ecd7059c34cf4c68886638208e57014e9
ThousandAI <ceo@thousandai.com>
2022-06-21 10:51:43 +0800

    class02 swap

---
+++ class02/src/class02.c
@@ -0,0 +1,28 @@
+/*
+ ============================================================================
+ Name        : class02.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+int main(void) {
+	setbuf(stdout,NULL);
+	int x,y,temp;
+	printf("x: ");
+	scanf("%d", &x);
+	printf("y: ");
+	scanf("%d", &y);
+	temp = x;
+	x = y;
+	y = temp;
+	printf("x: %d\n",x);
+	printf("y: %d",y);
+
+	return 0;
+
+}
