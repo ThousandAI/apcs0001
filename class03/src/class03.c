3e91c409ed68c544686590f5ea889cf848f169f4
ThousandAI <ceo@thousandai.com>
2022-06-22 18:38:36 +0800

    class03.c editing

--- class03/src/class03.c
+++ class03/src/class03.c
@@ -42,12 +42,58 @@ int main(void) {
 	printf("The absolute value of %d is %d", x, k);
 	*/
 
+	// 使用三元運算子找尋最大值
+	/*
 	int x, y, z;
 	scanf("%d%d%d", &x, &y, &z);
 	int max = (x > y) ? x : y;
 	max = (z > max) ? z : max;
 	printf("%d", max);
 	return 0;
+	*/
+
+	// switch
+	/*
+	char grade;
+	scanf("%c", &grade);
+	switch (grade){
+		case 'A':
+			printf("You Got %c.\n", grade);
+			printf("Excellent!!");
+			break;
+		case 'B':
+			printf("You Got %c.\n", grade);
+			printf("Well done.");
+			break;
+		case 'F':
+			printf("You Got %c.\n", grade);
+			printf("You failed");
+			break;
+		default:
+			printf("Please Enter a valid grade");
+	}
+	*/
+
+	// while
+	/*
+	int num;
+	int count = 0;
+	scanf("%d", &num);
+	while(count < num){
+		printf("%d ", count);
+		count++;
+	}
+	*/
+
+	// 總和
+	int num;
+	int sum = 0,count = 1;
+	scanf("%d", &num);
+	while(count <= num){
+		sum += count;
+		count ++;
+	}
+	printf("%d", sum);
 
 
 
@@ -56,9 +102,7 @@ int main(void) {
 
 
 
-
-
-
+	return 0;
 
 
 
