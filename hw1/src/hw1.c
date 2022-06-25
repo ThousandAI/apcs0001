cf6a07b95d0a0ab2be48c773a99637b3a252d7b9
ThousandAI <ceo@thousandai.com>
2022-06-25 23:17:59 +0800

    hw1.c done

---
+++ hw1/src/hw1.c
@@ -0,0 +1,68 @@
+/*
+ ============================================================================
+ Name        : hw1.c
+ Author      : Thousand AI
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+int main(void) {
+
+	/*
+	1.
+	*/
+
+	/*
+	2.
+	*/
+
+	/*
+	3.
+	*/
+
+	/*
+	4.
+	*/
+
+	/*
+	5.
+	*/
+
+	/*
+	6.
+	*/
+
+	/*
+	7.
+	*/
+
+	/*
+	8.
+	int number;
+	scanf("%d", &number);
+	// Write your code here
+	*/
+
+	/*
+	9.
+	int weight,height;
+	float bmi;
+	scanf("%d %d", &weight, &height);
+	// Write your code here
+	*/
+
+	/*
+	10.
+	int seconds;
+	scanf("%d", &seconds);
+	// Write your code here
+	*/
+
+
+
+	return 0;
+}
