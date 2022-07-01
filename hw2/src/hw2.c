fb77df1b9a6dd181d6ae4fe2d652f5b264d50a81
ThousandAI <ceo@thousandai.com>
2022-07-01 11:48:55 +0800

    hw2.c done

--- hw2/src/hw2.c
+++ hw2/src/hw2.c
@@ -14,16 +14,24 @@
 int main(void) {
 	/*
 	1.
+	(1)
+	(2)
+	(3)
+	(4)
 	*/
 
 	/*
 	2.
 	*/
 
-	/*
-	3.
-	for(int i = 0; i < 6; i++){
 
+	//3.
+	/*
+	for(int i = 0; i < 6; i++){
+		float temp;
+		scanf("%f", &temp);
+		// write your code here
+		printf("%.1f ", temp);
 	}
 	*/
 
@@ -35,16 +43,35 @@ int main(void) {
 	5.
 	*/
 
+
+	//6.
+	//提示: 假設最小值 101 最大值 -1，去比較如果比最小值小就將最小值替換掉，比較最大值大就將最大值替換掉
 	/*
-	6.
-	提示: 假設最小值 101 最大值 -1，去比較如果比最小值小就將最小值替換掉，比較最大值大就將最大值替換掉
+	int min = 101;
+	int max = -1;
+	for(int i = 0; i < 10; i++){
+		// write your code here
+	}
+	printf("最大值: %d\n", max);
+	printf("最小值: %d\n", min);
 	*/
 
+
+	//7.
 	/*
-	7.
+	int num;
+	int odd_sum = 0; // 奇數總和
+	int even_sum = 0; // 偶數總和
+	scanf("%d", &num);
+	// write your code here
 	*/
 
 	/*
 	8.
+	int n1, n2;
+	int gcd = 1; // 最大公因數
+	scanf("%d %d", &n1, &n2);
+	// write your code here
+	printf("%d", gcd);
 	*/
 }
