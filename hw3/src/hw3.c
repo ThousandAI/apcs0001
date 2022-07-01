ddf8054ba64327934a451c1c95b4656a99531f5f
ThousandAI <ceo@thousandai.com>
2022-07-01 11:33:13 +0800

    hw3.c done

---
+++ hw3/src/hw3.c
@@ -0,0 +1,72 @@
+/*
+ ============================================================================
+ Name        : hw3.c
+ Author      : Thousand AI
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <math.h>
+
+/*
+write your function here
+
+}
+*/
+
+
+int main(void){
+	/*
+	1.
+	(1)
+	(2)
+	*/
+
+	/*
+	2.
+	*/
+
+	/*
+	3.
+	*/
+
+	/*
+	4.
+	*/
+
+	/*
+	5.
+	*/
+
+	//6.
+	/*
+	int num;
+	scanf("%d", &num);
+	// Write your code here
+	// 提示: 在第一個迴圈內，分別有兩個迴圈，一個控制空格，一個控制 *
+	*/
+
+	//7.
+	/*
+	int num;
+	scanf("%d", &num);
+	// write your code here
+	*/
+
+	//8.
+	/*
+	double x1, y1, x2, y2;
+	scanf("%lf %lf %lf %lf", &x1, &y1, &x2, &y2);
+	double result:
+	// write your code here
+	printf("%.2f", ans);
+	*/
+
+	return 0;
+
+}
+
+
