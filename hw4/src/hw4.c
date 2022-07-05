6dadbba7a78620c6d7429e94570084844e4199fe
ThousandAI <ceo@thousandai.com>
2022-07-05 12:20:21 +0800

    hw4.c done

---
+++ hw4/src/hw4.c
@@ -0,0 +1,52 @@
+/*
+ ============================================================================
+ Name        : hw4.c
+ Author      : ThousandAI
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* 回答每題，越詳細越好
+
+ 3.
+
+ 4.
+
+ 5.
+
+ 6.
+
+*/
+
+
+
+// 7.
+/*
+int main(void) {
+	int matrix[3][5] = {{3,7,1,2,5},{3,2,10,5,1},{6,-1,-5,3,8}};
+
+	// write your code here
+
+}
+*/
+
+// 8.
+/*
+int main(void) {
+	int matrix[5][5] = {{3,2,3,1,7},
+						{1,5,4,6,2},
+						{8,6,3,2,1},
+						{6,7,8,7,6},
+						{5,3,10,3,1}};
+
+	// write your code here
+
+}
+*/
+
+
