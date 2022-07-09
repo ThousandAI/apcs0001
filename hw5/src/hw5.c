ed8c74f7eac0d7fa3aed314f9cf953586b8f5e66
ThousandAI <ceo@thousandai.com>
2022-07-09 18:48:39 +0800

    hw5.c done

---
+++ hw5/src/hw5.c
@@ -0,0 +1,59 @@
+/*
+ ============================================================================
+ Name        : hw5.c
+ Author      : Thousand AI
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+/*
+	 1.
+
+	 2.
+
+	 3.
+
+	 4.
+*/
+
+
+// 5.
+/*
+#include <stdio.h>
+
+int get_max(int *parr, int size){
+	int max = *parr; // max = parr[0]
+	// Write your code here
+
+
+	return max;
+}
+
+int main(void){
+	int arr[10] = {32, 58, 93, 67, 21, 88, 95, 100, 83, 78};
+	int max = get_max(arr, 10);
+	printf("%d", max);
+}
+*/
+
+// 6.
+/*
+#include <stdio.h>
+
+int get_length(char *p){
+	int ans = 0;
+	// Write your code here, hint: while loop
+
+	return ans;
+}
+
+int main(void){
+	char s[] = {"Welcome To APCS0001!!"};
+	int length = get_length(s);
+	printf("%d", length);
+
+}
+*/
+
